1d5b39785366ecd03c46805501405c55928c9495
Felipe <feliperpdr@yahoo.com.br>
2019-11-11 22:24:55 -0300

    Add Server Streaming API implementation.

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,47 +1,75 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"net"
+  "bytes"
+  "context"
+  "log"
+  "net"
+  "strconv"
+  "time"
 
-	"github.com/_dev/grpc-go-example/greet/greetpb"
+  "github.com/_dev/grpc-go-example/greet/greetpb"
 
-	"google.golang.org/grpc"
+  "google.golang.org/grpc"
 )
 
 type server struct{}
 
+// Unary API
 func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
-	fmt.Printf("Greet function was invoked with %v", req)
-	firstName := req.GetGreeting().GetFirstName()
-	result := "Hello " + firstName
-	resp := &greetpb.GreetResponse{
-		Result: result,
-	}
-	return resp, nil
+  log.Printf("Greet function was invoked with %v", req)
+
+  firstName := req.GetGreeting().GetFirstName()
+  var buffer bytes.Buffer
+  buffer.WriteString("Hello ")
+  buffer.WriteString(firstName)
+
+  response := &greetpb.GreetResponse{
+    Result: buffer.String(),
+  }
+  return response, nil
+}
+
+// Server Streaming API
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
+  log.Printf("GreetManyTimes function was invoked with %v", req)
+
+  firstName := req.GetGreeting().GetFirstName()
+  for i := 0; i < 10; i++ {
+    var buffer bytes.Buffer
+    buffer.WriteString("Hello ")
+    buffer.WriteString(firstName)
+    buffer.WriteString(" number ")
+    buffer.WriteString(strconv.Itoa(i))
+
+    response := &greetpb.GreetManyTimesResponse{
+      Result: buffer.String(),
+    }
+    stream.Send(response)
+    time.Sleep(1000 * time.Millisecond) // wating 1 second.
+  }
+  return nil
 }
 
 func main() {
-	fmt.Println("Server starting...")
+  log.Println("SERVER - starting...")
 
-	// Creating the port of GRPC server...
-	list, err := net.Listen("tcp", "0.0.0.0:50051")
-	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
-	}
+  // Creating the port of GRPC server...
+  list, err := net.Listen("tcp", "0.0.0.0:50051")
+  if err != nil {
+    log.Fatalf("Failed to listen: %v", err)
+  }
 
-	// Creating GRPC server...
-	s := grpc.NewServer()
+  // Creating GRPC server...
+  s := grpc.NewServer()
 
-	// Registring de GreetService in GRPC server...
-	greetpb.RegisterGreetServiceServer(s, &server{})
+  // Registring de GreetService in GRPC server...
+  greetpb.RegisterGreetServiceServer(s, &server{})
 
-	fmt.Println("Server running...")
+  log.Println("SERVER - running...")
 
-	// Binding the port to GRPC server...
-	if err := s.Serve(list); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
-	}
+  // Binding the port to GRPC server...
+  if err := s.Serve(list); err != nil {
+    log.Fatalf("Failed to serve: %v", err)
+  }
 }
